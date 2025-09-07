daeff2c473c6c74e59e31e8c50dd9546ead78b7a
Clive Jaden <clivejaden04@gmail.com>
2025-09-08 02:35:55 +0530

    Add FastAPI upload code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
 import os
 from datetime import datetime
+import glob
 
 app = FastAPI()
 
@@ -20,3 +22,11 @@ async def upload_image(request: Request):
         f.write(data)
 
     return {"message": "Uploaded", "path": file_path}
+
+@app.get("/latest")
+def get_latest_image():
+    files = glob.glob(os.path.join(UPLOAD_DIR, "*.jpg"))
+    if not files:
+        return {"message": "No images yet"}
+    latest_file = max(files, key=os.path.getctime)
+    return FileResponse(latest_file, media_type="image/jpeg")
